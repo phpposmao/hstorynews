fafb5c97d80d649485e17d7b87ccc50926323ba3
phpposmao <phpposmao@gmail.com>
2022-03-17 17:50:17 -0300

    Reformed app, added posts page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { ActiveLink } from '../ActiveLink';
+
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <h1>spfc.news</h1>
+        <h1>hstory.news</h1>
+
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href='/'>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href='/posts'>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,27 +1,30 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       type='button'
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="var(--red-100)" />
-      Usuário Teste
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type='button'
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
-      Usuário Teste
+      Sign in with github
     </button>
   )
 }
